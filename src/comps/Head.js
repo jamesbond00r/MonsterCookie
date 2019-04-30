6d765cb71e2b684104c5015bb50d4d6ab40fbbb8
Robert Clark <rob@associated.local>
2019-04-30 11:46:41 -0500

    change style

--- src/comps/Head.js
+++ src/comps/Head.js
@@ -3,14 +3,17 @@ import React from 'react';
 
 function Head() {
   return (
+<div>
+  	<h1 className="pizza">Pizza App!</h1>
 <div id="jumbo">
 
 <div>
     <ul className="navbar">
-  <li id="raphael"><a href="default.asp">Home</a></li>
-  <li id="donatello"><a href="news.asp">News</a></li>
-  <li id="leonardo"><a href="contact.asp">Contact</a></li>
-  <li id="michelangelo"><a href="about.asp">About</a></li>
+  <li id="raphael"><a href="default.asp">Raphael</a></li>
+  <li id="donatello"><a href="news.asp">Donatello</a></li>
+  <li id="leonardo"><a href="contact.asp">Leonardo</a></li>
+  <li id="michelangelo"><a href="about.asp">Michelangelo</a></li>
+
 </ul>
     </div>
 
@@ -19,6 +22,7 @@ function Head() {
   	
   	
 </div>
+    </div>
     </div>
   );
 }
